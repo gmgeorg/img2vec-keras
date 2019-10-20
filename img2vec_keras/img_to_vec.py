6aba19fee6357ea0887d3320f4f6b9907e594aed
Georg <im@gmge.org>
2019-10-20 10:30:51 -0400

    add a .transform() method to use in sklearn pipelines

--- img2vec_keras/img_to_vec.py
+++ img2vec_keras/img_to_vec.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from typing import Text, Optional
+
 from tensorflow import keras
 
 from tensorflow.keras.applications import resnet50
@@ -14,27 +16,48 @@ _IMAGE_NET_TARGET_SIZE = (224, 224)
 
 class Img2Vec(object):
 
-    def __init__(self):
+    def __init__(self, layer_name: Text = "avg_pool"):
         
         model = resnet50.ResNet50(weights='imagenet')
-        layer_name = 'avg_pool'
+        self._layer_name = layer_name
         self.intermediate_layer_model = Model(inputs=model.input, 
-                                              outputs=model.get_layer(layer_name).output)
+                                              outputs=model.get_layer(self._layer_name).output)
 
-
-    def get_vec(self, image_path):
+    def get_vec(self, image_path: Optional[Text] = None, img=None) -> np.ndarray:
         """ Gets a vector embedding from an image.
+
         :param image_path: path to image on filesystem
+        :param img: image object (Pillow). If image_path is not provided, then
+          img must be provided.
+
         :returns: numpy ndarray
         """
+        if img is None:
+            img = image.load_img(image_path, 
+                                 target_size=_IMAGE_NET_TARGET_SIZE)
+        else:
+            img = img.resize(_IMAGE_NET_TARGET_SIZE)
+            if img.mode != "RGB":
+                # Convert it to 3-channel image.
+                img = img.convert("RGB")
 
-        img = image.load_img(image_path, target_size=_IMAGE_NET_TARGET_SIZE)
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = resnet50.preprocess_input(x)
-        intermediate_output = self.intermediate_layer_model.predict(x)
+        return self.transform(image.img_to_array(img))
+    
+    def transform(self, X: np.ndarray) -> np.ndarray:
+        """ Gets a vector embedding from an image array.
+
+        The .transform() method follows sklearn-style and can therefore be used
+        in sklearn Pipeline.
+        
+        :param X: image encoded as numpy array.
+
+        :returns: The embedding of the image as numpy ndarray
+        """
+        intermediate_output = self.intermediate_layer_model.predict(
+            resnet50.preprocess_input(np.expand_dims(X, axis=0)))
         
         return intermediate_output[0]
-    
+
+
 if __name__ == "main":
      pass    
